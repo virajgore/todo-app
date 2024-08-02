790e88296e72cb3eb5f5ecf0d0f2b1dadb28c704
virajgore <virajgore24@gmail.com>
2024-08-02 18:46:47 +0530

    Changed db.js file

--- todo-app/db.js
+++ todo-app/db.js
@@ -5,7 +5,16 @@
 // eslint-disable-next-line no-undef
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/")
+mongoose.connect("mongodb://localhost:27017/todos")
+
+mongoose.connection.on('connected', () => {
+    console.log("Mongoose connected to MongoDB");
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("Mongoose connection error:", err);
+});
+
 const todoSchema = mongoose.Schema(
 {
     title: String,
